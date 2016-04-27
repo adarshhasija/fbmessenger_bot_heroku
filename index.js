e15a2c9416393729682d09e8914a19fd69e11f7f
FAIMAC <faimac@FAIs-MacBook-Pro.local>
2016-04-27 22:43:35 +0530

    fb first message

--- index.js
+++ index.js
@@ -21,6 +21,43 @@ app.get('/webhook', function (req, res) {
 	}
 });
 
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+    }
+  }
+  res.sendStatus(200);
+});
+
+var token = "CAAW44q2oO0ABANZAmtQ6by1a7W2ljjZBLwzPwB5hVvgZCtyvLi4pZAwEmm2PGqDM8B801ueMT9vLsXDrSmTyGGixTR6EwkQjulRHhSYUQOcjsqXTBruMlZALMD0bS8iqouGpWMufokhAAUotPPLvp4tbAzne7Wgr6hUctlmSLOhAnaWnaAZAHiEjsxC9ZBl2ZBgZD";
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
