2517dbb9b755522c3b58fe7d0c9cd62e93e8f263
FAIMAC <faimac@FAIs-MacBook-Pro.local>
2016-04-29 01:25:59 +0530

    /webhook/

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', function(request, response) {
 });
 */
 
-app.get('/webhook', function (req, res) {
+app.get('/webhook/', function (req, res) {
 	if (req.query['hub.verify_token'] === '1234') {
 		res.send(req.query['hub.challenge']);
 	} else {
@@ -23,7 +23,7 @@ app.get('/webhook', function (req, res) {
 	}
 });
 
-app.post('/webhook', function (req, res) {
+app.post('/webhook/', function (req, res) {
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
