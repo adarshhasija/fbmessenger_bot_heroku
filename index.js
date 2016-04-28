97d3045b0c81cc0c199f0978d3c641507a02ef8a
FAIMAC <faimac@FAIs-MacBook-Pro.local>
2016-04-29 01:21:12 +0530

    abc

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+/*
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+*/
 
 app.get('/webhook', function (req, res) {
 	if (req.query['hub.verify_token'] === '1234') {
