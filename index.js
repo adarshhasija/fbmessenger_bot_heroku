10105b50bc146e2ddb252e10b71c2b1f4c6407b7
FAIMAC <faimac@FAIs-MacBook-Pro.local>
2016-04-29 17:13:58 +0530

    hello world

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//var bodyParser = require('body-parser')
-//var request = require('request')
+var bodyParser = require('body-parser')
+var request = require('request')
 var express = require('express')
 var app = express()
 
