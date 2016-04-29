d31e7c927415d7de199e2e11128b5f2030a832f8
FAIMAC <faimac@FAIs-MacBook-Pro.local>
2016-04-29 17:12:56 +0530

    hello world

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var bodyParser = require('body-parser')
-var request = require('request')
+//var bodyParser = require('body-parser')
+//var request = require('request')
 var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000));
 
 // Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+//app.use(bodyParser.urlencoded({extended: false}))
 
 // Process application/json
-app.use(bodyParser.json())
+//app.use(bodyParser.json())
 
 
 app.use(express.static(__dirname + '/public'));
