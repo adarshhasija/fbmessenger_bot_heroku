71f7c626afc0d3f1ebb70552c56f2e331a41b946
FAIMAC <faimac@FAIs-MacBook-Pro.local>
2016-05-02 23:29:20 +0530

    hello world

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.listen(app.get('port'), function() {
 });
 
 app.post('/webhook/', function (req, res) {
+	console.log('Test 1')
     messaging_events = req.body.entry[0].messaging
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i]
