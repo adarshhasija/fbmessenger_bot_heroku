4edafd38ecf97de8cb12b6b5bee3dfc8930bbb15
FAIMAC <faimac@FAIs-MacBook-Pro.local>
2016-05-03 15:19:01 +0530

    hello world

--- index.js
+++ index.js
@@ -50,7 +50,6 @@ app.post('/webhook/', function (req, res) {
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i]
         sender = event.sender.id
-        console.log(event.message)
         if (event.message && event.message.text) {
             text = event.message.text
             if (text === 'Generic') {
@@ -91,6 +90,8 @@ function sendWelcomeMessage(text) {
       console.log('Error sending message: ', error);
     } else if (response.body.error) {
       console.log('Error: ', response.body.error);
+    } else {
+    	sendTextMessage("The next line")
     }
   });
 };
