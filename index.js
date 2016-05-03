2589de387154a419f3e4ab74bcc153d5c29fad20
FAIMAC <faimac@FAIs-MacBook-Pro.local>
2016-05-03 17:18:37 +0530

    hello world

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/', function(req, res) {
 var token = "EAAW44q2oO0ABAMtYPDZCNh0DINSOfffzT6a3U7wGieMxPDGSxwzxX6w4Xz7TtQWrsKaqsZCWNzmmRBmoNDtosiC1lsNRVRLbsKM4eO4ZAxEdBTktURvyDqJm5YWY1O16fjgZCHs5k4SofZCMEZC0qbY8YDYI3xMjdAN8FpL2vlmQZDZD";
 var PAGE_ID = "188138181333428"; 
 var app_name = "Cila"
-var state = '';
+var state = 'welcome_0';
 
 
 app.get('/webhook/', function (req, res) {
@@ -62,9 +62,18 @@ app.post('/webhook/', function (req, res) {
             sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
             continue
         }
-        if (state == '') {
-        	sendTextMessage(sender, 'Next text')
-        	state = 'Hi'
+        if (state == 'welcome_0') {
+        	sendTextMessage(sender, 'We connect volunteers with the visually impaired community in Bangalore')
+        	state = 'welcome_1'
+        }
+        if (state == 'welcome_1') {
+        	sendTextMessage(sender, 'Volunteers can work as scribes as well as tutors. Scribes write exams for the students '+
+        							'while tutors read and teach different subjects')
+        	state = 'welcome_2'
+        }
+        if (state == 'welcome_2') {
+        	sendTextMessage(sender, 'Would you like to register as a volunteer? Please reply yes or no.')
+        	state = 'welcome_complete'
         }
     }	
     res.sendStatus(200)
