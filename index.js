bd28a4bfed712a2f37ab383ec5eadee1a24943f8
FAIMAC <faimac@FAIs-MacBook-Pro.local>
2016-06-28 23:23:09 +0530

    4 buttons

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.post('/webhook/', function (req, res) {
         if (event.message && event.message.text) {
             text = event.message.text
             sendStructuredTextMessage("Choose a word to get the sign language", 
-              ["Hello", "Goodbye", "Nice to meet you"])
+              ["Hello", "Goodbye", "Nice to meet you", "Yes"])
             //sendTextMessage(sender, "Sorry, I did not understand your reponse. Please try again.");
         }
         if (event.postback) {
