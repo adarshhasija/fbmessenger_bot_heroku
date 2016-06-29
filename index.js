556352a2ef0dbbc3c1e266c11b8011e30f094e7c
FAIMAC <faimac@FAIs-MacBook-Pro.local>
2016-06-29 09:27:48 +0530

    added all isl

--- index.js
+++ index.js
@@ -240,7 +240,230 @@ function letterToISL(sender, letter) {
             sendGenericMessage(sender, elements)
           break
       case 'b':
-          
+          var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fb_isl.png?alt=media&token=cf006cc1-c02b-4597-a4f3-daaab1c3072a",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'c':
+          var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fc_isl.png?alt=media&token=d67d00b2-00de-4854-ad60-d348fc0ec4e9",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'd':
+            var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fd_isl.png?alt=media&token=83d2db2c-ddcc-469d-a4ac-960cee153fb0",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'e':
+            var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fe_isl.png?alt=media&token=a59d77eb-fbe2-44c5-9cb4-f7f88b13074f",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'f':
+            var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Ff_isl.png?alt=media&token=cd2c4b0b-e793-4e04-bf53-af5899309da1",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'g':
+            var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fg_isl.png?alt=media&token=ba2efb29-15f6-460d-9cd3-6dc449e4497d",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'h':
+            var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fh_isl.png?alt=media&token=95172ccb-3dfa-4671-8449-5ac387162006",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'i':
+            var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fi_isl.png?alt=media&token=1ba4be80-87e2-4c2e-9710-49a60028ceb0",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'j':
+            var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fj_isl.png?alt=media&token=bbeb352c-5deb-4eaa-9525-457fee2d7e80",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'k':
+          var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fk_isl.png?alt=media&token=6f72f2ee-1c2b-4c97-95b5-d56a520b1427",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'l':
+          var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fl_isl.png?alt=media&token=a5176bc7-ebc9-4842-9a79-fddd4292e0c1",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'm':
+          var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fm_isl.png?alt=media&token=f43d60cf-ee02-41f7-b370-6d3aa577dd55",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'n':
+          var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fn_isl.png?alt=media&token=af05b408-a1dd-40a3-9e08-a4e118a53009",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'o':
+          var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fo_isl.png?alt=media&token=7539ff08-2c6e-495f-83a6-34ea4fddeed0",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'p':
+          var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fp_isl.png?alt=media&token=158d1986-3b97-4698-a112-4a7540bc2ddc",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'q':
+          var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fq_isl.png?alt=media&token=fad9a01c-34e7-41de-9ef8-a9b5a617cf95",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'r':
+          var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fr_isl.png?alt=media&token=552d9d01-508f-4899-b3fa-2f170f31d472",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 's':
+          var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fs_isl.png?alt=media&token=e09aa80e-0d19-4e91-a6cc-d97bf23e21a9",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 't':
+          var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Ft_isl.png?alt=media&token=21e8e412-183f-42b4-a5b0-178cc120af42",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'u':
+          var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fu_isl.png?alt=media&token=1b73e175-f8da-4da9-96a8-513d25206212",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'v':
+          var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fv_isl.png?alt=media&token=f746d54a-f26f-40e0-a7a0-cb6528e3fc50",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'w':
+            var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fw_isl.png?alt=media&token=8158f6af-dc4b-4024-a3fd-01b360ce0e81",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'x':
+            var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fx_isl.png?alt=media&token=3065a954-eb31-4649-8d52-aec052d27cd3",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'y':
+            var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fy_isl.png?alt=media&token=7b027197-6c2d-46f5-aa65-f0ccc521a8ff",
+                })
+            sendGenericMessage(sender, elements)
+            break
+      case 'z':
+            var elements = []
+            elements.push({
+                    "title": letter.toUpperCase(),
+                    "subtitle": "Indian Sign Language",
+                    "image_url": "https://firebasestorage.googleapis.com/v0/b/cila-1.appspot.com/o/isl_alphabet%2Fz_isl.png?alt=media&token=03e12c87-d3eb-410a-92f8-268c898757e9",
+                })
+            sendGenericMessage(sender, elements)
+            break
       default:
           break
     }
