68a56cc2100256b28256ce6b065a6d876501a041
FAIMAC <faimac@FAIs-MacBook-Pro.local>
2016-07-06 19:27:40 +0530

    removed config

--- index.js
+++ index.js
@@ -26,6 +26,22 @@ app.get('/', function(req, res) {
   res.send('hello world, I am a chat bot')
 });
 
+/*
+ * Use your own validation token. Check that the token used in the Webhook 
+ * setup is the same token used here.
+ *
+ */
+app.get('/webhook/', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
 
 /*
  * Verify that the callback came from Facebook. Using the App Secret from 
