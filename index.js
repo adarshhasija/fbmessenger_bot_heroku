94aae14b87131d89fc95ecd68ff4b6da7a9869e5
FAIMAC <faimac@FAIs-MacBook-Pro.local>
2016-07-06 19:06:44 +0530

    hello world

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ const
 var app = express();
 
 app.set('port', process.env.PORT || 5000);
-app.use(bodyParser.json({ verify: verifyRequestSignature }));
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'));
 
 
 
