e5ec1127e4d8e78f65a74ee5dd61a8cd47adb809
FAIMAC <faimac@FAIs-MacBook-Pro.local>
2016-07-06 19:10:40 +0530

    hello world

--- index.js
+++ index.js
@@ -12,11 +12,40 @@ app.use(bodyParser.json())
 app.use(express.static('public'));
 
 
+/*
+ * Be sure to setup your config values before running this code. You can 
+ * set them using environment variables or modifying the config file in /config.
+ *
+ */
+
+// App Secret can be retrieved from the App Dashboard
+const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ? 
+  process.env.MESSENGER_APP_SECRET :
+  config.get('appSecret');
+
+// Arbitrary value used to validate a webhook
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+  (process.env.MESSENGER_VALIDATION_TOKEN) :
+  config.get('validationToken');
+
+// Generate a page access token for your page from the App Dashboard
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+  config.get('pageAccessToken');
+
+if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN)) {
+  console.error("Missing config values");
+  process.exit(1);
+}
+
 
 app.get('/', function(req, res) {
   //res.render('pages/index');
   res.send('hello world, I am a chat bot')
 });
+
+
+
 
 
 // Start server
