ee4c24d3896e553f10b1c7ec53bfedf8a37bb610
FAIMAC <faimac@FAIs-MacBook-Pro.local>
2016-07-06 19:04:38 +0530

    hello world

--- index.js
+++ index.js
@@ -21,3 +21,12 @@ app.get('/', function(req, res) {
   res.send('hello world, I am a chat bot')
 });
 
+
+// Start server
+// Webhooks must be available via SSL with a certificate signed by a valid 
+// certificate authority.
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+module.exports = app;
