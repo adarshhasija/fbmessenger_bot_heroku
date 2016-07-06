4bb68b388f31d5e1d80a0487622d268b083759d0
FAIMAC <faimac@FAIs-MacBook-Pro.local>
2016-07-06 19:25:48 +0530

    removed config

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const
 var app = express();
 
 app.set('port', process.env.PORT || 5000);
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
 
 //Details for Cila
@@ -26,6 +25,37 @@ app.get('/', function(req, res) {
   //res.render('pages/index');
   res.send('hello world, I am a chat bot')
 });
+
+
+/*
+ * Verify that the callback came from Facebook. Using the App Secret from 
+ * the App Dashboard, we can verify the signature that is sent with each 
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an 
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+                        .update(buf)
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
 
 
 
