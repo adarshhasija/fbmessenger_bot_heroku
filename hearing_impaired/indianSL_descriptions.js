855e6a1d0efe238a97f9fcfa9b7d18da4ef18d86
FAIMAC <faimac@FAIs-MacBook-Pro-2.local>
2016-07-09 22:24:13 +0530

    hello world

---
+++ hearing_impaired/indianSL_descriptions.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    const a_description = "Raise both your hands with fingers pointing up and palms pointing away from you. Extend both your thumbs so that"+
+                                    "the tips are touching. Finally curl all your other fingers inwards";
+};
+
