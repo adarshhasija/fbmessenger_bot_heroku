17f7bab4105b034240963dba365d2db0faa18fb9
FAIMAC <faimac@FAIs-MacBook-Pro-2.local>
2016-07-09 21:59:15 +0530

    hello world

--- index.js
+++ index.js
@@ -189,9 +189,10 @@ function receivedMessage(event) {
     messageText = messageText.toLowerCase()
     if (messageText.length == 1) {
           //var description = indianSL_letters.letterToISLDescription(messageText);
-          //sendTextMessage(senderID, description)
-          var imageURL = indianSL_letters.letterToISLImage(messageText);
-          sendImageMessage(senderID, imageURL);
+          var description = indianSL_letters.letterToISLImage(messageText);
+          sendTextMessage(senderID, description)
+          //var imageURL = indianSL_letters.letterToISLImage(messageText);
+          //sendImageMessage(senderID, imageURL);
     }
     else {
       sendTextMessage(senderID, DEFAULT_MESSAGE)
